46b06693b96e2551e5121ad6eb024dcd84ddb075
GET Academy <terje@kolderup.net>
2024-05-30 09:21:47 +0200

    x

---
+++ FlexibleList/OldProgram.cs
@@ -0,0 +1,21 @@
+﻿namespace FlexibleList
+{
+    internal class OldProgram
+    {
+        public static void Run()
+        {
+            var numbers = new int[4];
+            var count = 0;
+            while (true)
+            {
+                var numberStr = Console.ReadLine();
+                var number = Convert.ToInt32(numberStr);
+                var index = count;
+                numbers[index] = number;
+                count++;
+
+                Console.WriteLine($"Count={count} Numbers={string.Join(',', numbers)}");
+            }
+        }
+    }
+}
---
+++ FlexibleList/Program.cs
@@ -0,0 +1,12 @@
+﻿var numbers = new int[4];
+var count = 0;
+while (true)
+{
+    var numberStr = Console.ReadLine();
+    var number = Convert.ToInt32(numberStr);
+    var index = count;
+    numbers[index] = number;
+    count++;
+
+    Console.WriteLine($"Count={count} Numbers={string.Join(',', numbers)}");
+}
