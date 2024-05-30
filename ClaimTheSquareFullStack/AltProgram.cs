8395a533196e55f3bff028644dc8d9ad70aecc1b
GET Academy <terje@kolderup.net>
2024-05-30 13:37:35 +0200

    x

---
+++ ClaimTheSquareFullStack/AltProgram.cs
@@ -0,0 +1,41 @@
+﻿using ClaimTheSquareFullStack.Model;
+
+namespace ClaimTheSquareFullStack
+{
+    public class AltProgram
+    {
+        public static void Mainx(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+            app.UseHttpsRedirection();
+            //app.MapGet("/terje", GetPerson);
+            app.MapGet("/terje", () =>
+            {
+                return new Person
+                {
+                    Name = "Terje",
+                    Email = "terje@getacademy.no"
+                };
+            });
+
+            app.MapPost("/terje", (Person p) =>
+            {
+                Console.WriteLine($"{p.Name} {p.Email}" );
+            });
+            app.UseStaticFiles();
+            app.Run();
+        }
+
+        /*
+           private static Person GetPerson()
+           {
+             return new Person
+             {
+               Name = "Terje",
+               Email = "terje@getacademy.no"
+             };
+           }
+         */
+    }
+}
--- ClaimTheSquareFullStack/Program.cs
+++ ClaimTheSquareFullStack/Program.cs
@@ -1,40 +1,20 @@
 using ClaimTheSquareFullStack.Model;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.MapGet("/terje", () =>
 {
-    public static void Main(string[] args)
+    return new Person
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
-        app.UseHttpsRedirection();
-        //app.MapGet("/terje", GetPerson);
-        app.MapGet("/terje", () =>
-        {
-            return new Person
-            {
-                Name = "Terje",
-                Email = "terje@getacademy.no"
-            };
-        });
+        Name = "Terje",
+        Email = "terje@getacademy.no"
+    };
+});
 
-        app.MapPost("/terje", (Person p) =>
-        {
-            Console.WriteLine($"{p.Name} {p.Email}" );
-        });
-        app.UseStaticFiles();
-        app.Run();
-    }
-
-    /*
-       private static Person GetPerson()
-       {
-         return new Person
-         {
-           Name = "Terje",
-           Email = "terje@getacademy.no"
-         };
-       }     
-     */
-}
-
-
+app.MapPost("/terje", (Person p) =>
+{
+    Console.WriteLine($"{p.Name} {p.Email}");
+});
+app.UseStaticFiles();
+app.Run();
